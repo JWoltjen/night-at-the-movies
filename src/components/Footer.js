9193d9829706bf0b788d97caf6e8425b709cc8b1
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-18 14:20:39 -0600

    create footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+background-color: #001219; 
+height: 5rem; 
+width: 100%; 
+padding: 0px 40px; 
+display: flex; 
+align-items: center; 
+justify-content: space-between; 
+position: fixed; 
+bottom: 0px; 
+`
+
+const Title = styled.h1`
+color: #e9d8a6; 
+font-size: 20px; 
+font-weight: 600; 
+`
+
+const Description = styled.p`
+color: #e9d8a6; 
+font-size: 20px; 
+font-weight: 600; 
+
+`
+
 
 function Footer() {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Title>This is a footer</Title>
+            <Description>Still just a footer</Description>
+        </Container>
     )
 }
 
