a9d7e13e6bba5d259f9f690cab0ccb42035c4151
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-18 14:31:53 -0600

    style Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+import MovieListing from './MovieListing'
+
+const Banner = styled.img`
+
+`
+
+const Container = styled.div`
+    width: 100%; 
+    
+`
 
 function Home() {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Banner/>
+            <MovieListing/>
+        </Container>
     )
 }
 
