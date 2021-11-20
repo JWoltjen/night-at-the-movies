305a1c9bc5aba2cd371e0a6fb3836c9408c518f0
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-20 12:52:24 -0600

    edit responsive settings for carousel

--- src/features/Settings.js
+++ src/features/Settings.js
@@ -2,18 +2,18 @@ export const Settings = {
         dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 4,
+        slidesToShow: 3,
         slidesToScroll: 3, 
         responsive: [
             {
-              breakpoint: 1500, 
+              breakpoint: 1450, 
               settings: {
-                  slidesToShow: 3, 
+                  slidesToShow: 6, 
                   slidesToScroll: 3, 
                   inifinte: true, 
                   dots: false
               },
-              breakpoint: 1025,
+              breakpoint: 1023,
               settings: {
                 slidesToShow: 3,
                 slidesToScroll: 1,
@@ -22,9 +22,17 @@ export const Settings = {
               }
             },
             {
+                breakpoint: 815,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                  initialSlide: 1
+                }
+            }, 
+            {
               breakpoint: 600,
               settings: {
-                slidesToShow: 2,
+                slidesToShow: 1,
                 slidesToScroll: 1,
                 initialSlide: 1
               }
